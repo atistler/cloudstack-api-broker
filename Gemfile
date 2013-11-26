8be9ba47c2d39a3f7924a23c79c75cc23dcacb98
Adam Tistler <atistler@datapipe.com>
2013-11-26 15:37:05 -0500

    stage

--- Gemfile
+++ Gemfile
@@ -2,9 +2,11 @@ source 'https://rubygems.org'
 
 gem 'puma'
 gem 'sinatra'
+gem 'capistrano-rvm', '~> 0.1.0'
+gem 'capistrano-bundler'
+gem 'thin'
 
 group :development do
   gem 'capistrano'
-  gem 'capistrano-puma', require: false
 end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,48 @@
-# require 'airbrake/capistrano'
-require 'bundler/capistrano'
-require 'capistrano-puma'
+set :application, 'cloudstack-api-broker'
+set :repo_url, 'git@github.com:atistler/cloudstack-api-broker.git'
 
-set :application, 'cab'
-set :repository,  'git@minerva.dpcloud.com:cloud-products/cloudstack-api-broker.git'
-set :user, 'root'
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
 set :deploy_to, '/srv/app'
 set :scm, :git
-set :use_sudo, false
+set :user, 'root'
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
+set :ssh_options, {
+  forward_agent: true,
+  user: 'root'
+}
 
-# Figure out the name of the current local branch
-def current_git_branch
-  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
-  puts "*** Deploying branch #{branch}"
-  branch
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
+
+namespace :deploy do
+
+  task :start do
+    "bundle exec thin start -C config/thin-config.yml"
+  end
+
+  task :stop do
+    "bundle exec thin stop -C config/thin-config.yml"
+  end
+
+  task :restart do
+    "bundle exec thin restart -C config/thin-config.yml"
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
 end
-
